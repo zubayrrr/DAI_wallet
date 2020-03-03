4bc066e2911270a01051e925b2beb7cf867c76de
zubayrrr <abdulfaiz076@gmail.com>
2020-03-03 09:54:49 +0530

    last commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    "0xAc65373F6132f7AE164eCDA2a59454B4623de6B2",
+    "1000000000000000000000"
+  );
+};
